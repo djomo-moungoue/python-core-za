e2b65ca5776910ee418f3dad71e15d39856d2da6
djomo-moungoue <djomo.moungoue@gmail.com>
2022-12-16 21:25:55 +0100

    pdf2text.py

--- core_python_with_mosh/djomopdf/djomopdf/pdf2text.py
+++ core_python_with_mosh/djomopdf/djomopdf/pdf2text.py
@@ -1,4 +1,16 @@
-def convert() -> None:
+"""This module provides functions to convert a PDF to a TEXT"""
+
+def convert(path: str) -> None:
+    """
+    Convert the given pdf document to a text document\n\n
+
+    Paremeters: \n
+    path (str): The path to a PDF file.
+    \n\n
+
+    Return: \n
+    str: The content to the PDF file as text.
+    """
     print(f"{'pdf2text'}")
 
 convert()
